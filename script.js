da3b017a4b6f8a7ecef826144ce8e7f1953f4381
Ana Laura Gomes <106351060+AnaLauraGomes@users.noreply.github.com>
2023-09-18 11:27:08 -0300

    A

--- script.js
+++ script.js
@@ -51,14 +51,14 @@ function check() {
 
   for (pos of positions) {
     if (pos.every((item) => items.includes(item))) {
-      alert("O JOGADOR '" + playerLastMove + "' GANHOU!");
+      alert("O JOGADOR '" + playerLastMove + "' GANHOU, ICÔNICO!");
       init();
       return;
     }
   }
 
   if (selected.filter((item) => item).length === 9) {
-    alert("DEU EMPATE!");
+    alert("DEU EMPATE, PAIA");
     init();
     return;
   }
